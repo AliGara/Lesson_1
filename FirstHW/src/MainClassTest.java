12a57d863961e1d9263bd5e885327d76dd9b9fed
AliGaraLoc <prezident_alina@mail.ru>
2021-03-03 17:00:14 +0700

    Test to check that the number is greater than 45.

--- FirstHW/src/MainClassTest.java
+++ FirstHW/src/MainClassTest.java
@@ -2,10 +2,10 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MainClassTest extends MainClass{
+
     @Test
-    public void testGetLocalNumber() {
-        int expected = 14;
-        int actual = this.getLocalNumber();
-        Assert.assertTrue("The number != 14", actual == expected);
+    public void testGetClassNumber() {
+        int actual = this.getClassNumber();
+        Assert.assertTrue("Number less than <= 45.", actual > 45);
     }
 }
